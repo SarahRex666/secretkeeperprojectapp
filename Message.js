74b3659adc75c51788d63695b321cedd4a20170f
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-11-23 19:39:12 -0500

    clipboard

--- Message.js
+++ Message.js
@@ -9,34 +9,14 @@ import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useRoute } from "@react-navigation/native";
-
+import * as Clipboard from 'expo-clipboard';
 
 export default function Message({ navigation, user }) {
-  const [userNameInput, setUserNameInput] = useState("");
-  const [userMessage, setUserMessage] = useState("");
 
-  const route = useRoute();
+  const copyToClipboard = async () => {
+    await Clipboard.setStringAsync(`https://secretkeepernext.vercel.app/${user.id}`);
+  };
 
-    const copyToClipboard = () => {
-
-  }
-
-
-  function handleSubmit() {
-    fetch("https://secretkeeperproject.herokuapp.com/message", {
-      method: "POST",
-      withCredentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        user_id: user.id,
-        message: userMessage,
-      }),
-    })
-      .then((r) => r.json())
-      .then(navigation.navigate("Profile"));
-  }
 
   return (
     <View style={styles.container}>
